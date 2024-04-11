27ab5aba938474408945a4af8bc15991039dcfe1
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2024-04-11 19:03:27 +0530

    RM|Added JWT token

--- wame_server/views.py
+++ wame_server/views.py
@@ -1,40 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from .serializer import CustomUserSerializer
-from .models import CustomUser
-import json
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['GET'])
 def google_login(request):
   token=request.GET.get('tokenId')
   idinfo = id_token.verify_oauth2_token(token, requests.Request(), '168406420522-apkent5peb8549sooc0hnpshra28bhoq.apps.googleusercontent.com')
-  print(request.session.__dict__)
   if idinfo['email_verified']:
-    if request.session.get('user'):
-      return Response(request.session['user'])
-    else:
-      userData = CustomUser.objects.filter(email=idinfo['email']).first()
-      if userData:
-        serializer = CustomUserSerializer(userData)
-        serialized_data = serializer.data
-      else:
-        data ={
-          'email': idinfo['email'],
-          'name': idinfo['name'],
-          'picture': idinfo['picture'],
-        }
-        serializer = CustomUserSerializer(data=data)
-        if serializer.is_valid():
-          userData = serializer.save()
-          serialized_data = serializer.data
-        else:
-          return Response(serializer.errors, status=400)
-      request.session['user'] = serialized_data
-        
-      return Response(request.session)
+    try:
+      user = User.objects.get(email=idinfo['email'])
+    except User.DoesNotExist:
+      user = User()
+      user.username = idinfo['name']
+      # provider random default password
+      user.password = make_password(BaseUserManager().make_random_password())
+      user.email = idinfo['email']
+      user.save()
+    
+    token = RefreshToken.for_user(user)  # generate token without username & password
+    response = {}
+    response['username'] = user.username
+    response['access_token'] = str(token.access_token)
+    response['refresh_token'] = str(token)
+    return Response(response)
 
   return Response(status=200)
